962c495143909cf7387d42e8116966d750bcf30d
KitwanaSh <rodriguekitweze@gmail.com>
2022-12-15 14:59:54 +0200

    feat: add the sibling task

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds if a node is sibling or not
+ * @node: the ckecked node
+ * Return: The sibling node
+*/
+
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
